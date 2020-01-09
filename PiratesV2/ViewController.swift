ee0cdda1c29940090551de538523ddc7554ca21f
MattWheeler10 <matthewwheeler66@gmail.com>
2020-01-09 16:29:19 +0000

    matt

--- PiratesV2/ViewController.swift
+++ PiratesV2/ViewController.swift
@@ -14,7 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // MARK: Variables
+    
+    var gold: Int = 0
+    var currentCrewMembers =
+["Chance","Bruno","Toby","Damien","Rafael","Domenic","Weston","Edison","Chris","Grant","Cortez","Darren","Nicolas","Everette","Jason","Vicente","Noe","Ricky","Ivory","Thomas","Guillermo","Isreal","Ben","Winfred","Lincoln","Kareem","Antwan","Greg","Lawrence","Corey","Todd","Jeffery","Rayford","Julio","Manual","Chi","Bradford","Devon","Bryan","Andreas","Don","Erik","Bobbie","Tyree","Felipe","Clifton","Carrol","Kasey","Cliff"]
+    
+    
+    
+    // MARK: Outlets
 
-
+    
+    
+    // MARK: Functions
+    
+    
 }
+
 
