86ed8aeefaeeb392cf3c1ad48ab40db9866b9175
Frazer MacRostie <frazermac101@outlook.com>
2020-01-09 16:20:50 +0000

    main master start point

---
+++ PiratesV2/PiratesAndShips.swift
@@ -0,0 +1,28 @@
+//
+//  PiratesAndShips.swift
+//  PiratesV2
+//
+//  Created by Code Nation on 09/01/2020.
+//  Copyright © 2020 Code Nation. All rights reserved.
+//
+
+import Foundation
+
+class PirateCrew {
+    var CaptainsName: String?
+    var crewNumber: Int = 10
+    var morale: Int = 50
+    var skill: Int = 30
+    var experience: Int = 0
+    var energy: Int = 50
+    
+}
+
+class PirateShip {
+    var ShipsName: String?
+    var health: Int = 100
+    var speed: Int = 60
+    var ballast: Int = 0
+    var condition: Int = 50
+    
+}
